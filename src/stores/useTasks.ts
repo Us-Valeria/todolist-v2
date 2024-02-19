6fb60d96c5f6711bda07654ad7df933dbb4090d7
Valeria Granina <v_granina.04@mail.ru>
2024-02-19 22:44:35 +0700

    add localStorage

--- src/stores/useTasks.ts
+++ src/stores/useTasks.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { Task } from "../models/Task";
 
 type TasksStore = {
@@ -9,51 +10,55 @@ type TasksStore = {
   changeStatusTask: (taskId: string) => void;
 };
 
-export const useTasks = create<TasksStore>((set) => ({
-  tasks: [],
-
-  addTask: ({ text }: { text: string }) => {
-    if (text.trim()) {
-      set((state) => ({
-        tasks: [
-          ...state.tasks,
-          {
-            id: String(Date.now()),
-            text: text,
-            completed: false,
-          },
-        ],
-      }));
-    }
-  },
-
-  removeTask: (taskId) => {
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== taskId),
-    }));
-  },
-
-  changeTextTask: (value, taskId) => {
-    set((state) => ({
-      tasks: state.tasks.map((task) => {
-        if (task.id === taskId) {
-          return { ...task, text: value };
-        } else {
-          return task;
+export const useTasks = create(
+  persist<TasksStore>(
+    (set) => ({
+      tasks: [],
+      addTask: ({ text }: { text: string }) => {
+        if (text.trim()) {
+          set((state) => ({
+            tasks: [
+              ...state.tasks,
+              {
+                id: String(Date.now()),
+                text: text,
+                completed: false,
+              },
+            ],
+          }));
         }
-      }),
-    }));
-  },
+      },
 
-  changeStatusTask: (taskId) => {
-    set((state) => ({
-      tasks: state.tasks.map((task) => {
-        if (task.id === taskId) {
-          return { ...task, completed: !task.completed };
-        } else {
-          return task;
-        }
-      }),
-    }));
-  },
-}));
+      removeTask: (taskId) => {
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== taskId),
+        }));
+      },
+
+      changeTextTask: (value, taskId) => {
+        set((state) => ({
+          tasks: state.tasks.map((task) => {
+            if (task.id === taskId) {
+              return { ...task, text: value };
+            } else {
+              return task;
+            }
+          }),
+        }));
+      },
+
+      changeStatusTask: (taskId) => {
+        set((state) => ({
+          tasks: state.tasks.map((task) => {
+            if (task.id === taskId) {
+              return { ...task, completed: !task.completed };
+            } else {
+              return task;
+            }
+          }),
+        }));
+      },
+    }),
+    { name: "task", getStorage: () => localStorage }
+  )
+);
