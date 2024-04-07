2bc2ff2a9ec5930d3efd1cb45fd16010525d55ef
Valeria Granina <v_granina.04@mail.ru>
2024-04-07 18:01:22 +0700

    fix

--- front/src/features/Filter/index.tsx
+++ front/src/features/Filter/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import type { RadioChangeEvent } from 'antd';
 import { Radio } from 'antd';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FILTER } from '../../models/Filter';
 import { setFilter, selectFilter } from './filterSlice';
 
 function FilterTaskList() {
   const dispatch = useDispatch();
   const onChange = (e: RadioChangeEvent) => dispatch(setFilter(e.target.value));
+  const filter = useSelector(selectFilter);
 
   return (
-    <Radio.Group value={selectFilter} onChange={onChange}>
+    <Radio.Group value={filter} onChange={onChange}>
       {Object.values(FILTER).map((status) => (
         <Radio.Button key={status} value={status}>
           {status === 'ALL' && 'Все'}
